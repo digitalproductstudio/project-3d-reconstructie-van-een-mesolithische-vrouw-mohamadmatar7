2653fabc80885289609732c0fefe6edd93f8f57e
mohamadmatar7 <mohamad.matar@student.arteveldehs.be>
2025-04-23 19:24:09 +0200

    Intensity

--- src/components/WomanModel.jsx
+++ src/components/WomanModel.jsx
@@ -6,12 +6,14 @@ export default function WomanModel({ skinColor, hairColor, eyeColor, ...props })
 
   useEffect(() => {
     scene.traverse((child) => {
+      console.log('child', child.name);
       if (child.isMesh) {
         child.material = child.material.clone();
 
-        if (child.name === 'Object_5') child.material.color.set(skinColor); // huid
-        if (child.name === 'Object_7') child.material.color.set(hairColor);  // haar
-        if (child.name === 'Object_6') child.material.color.set(eyeColor);   // ogen
+        if (child.name === 'Object_8') child.material.color.set(skinColor); // huid
+        // if (child.name === 'Object_7') child.material.color.set(hairColor);  // haar
+        if (child.name === 'Object_4' || child.name === 'Object_6' || child.name === 'hair_line_1' || child.name === 'hair_1_0') child.material.color.set(hairColor); // haar
+        if (child.name === 'Sketchfab_Scene') child.material.color.set(eyeColor);   // ogen
       }
     });
   }, [scene, skinColor, hairColor, eyeColor]);
--- src/pages/ModelPage.jsx
+++ src/pages/ModelPage.jsx
@@ -4,9 +4,10 @@ import { OrbitControls } from '@react-three/drei';
 import WomanModel from '../components/WomanModel';
 
 export default function ModelPage() {
-  const [skinColor, setSkinColor] = useState('#dab28f');
+  const [skinColor, setSkinColor] = useState('#FFFFFF');
   const [hairColor, setHairColor] = useState('#3b2f2f');
-  const [eyeColor, setEyeColor] = useState('#5f9ea0');
+  const [eyeColor, setEyeColor] = useState('#000000');
+  const [intensity, setIntensity] = useState(1);
 
   return (
     <div className="w-full h-screen flex flex-col md:flex-row bg-gray-200">
@@ -43,14 +44,27 @@ export default function ModelPage() {
                 className="w-full h-12 rounded-md border border-gray-300 shadow-inner cursor-pointer transition hover:scale-[1.02]"
                 />
             </div>
+
+            <div className="w-full flex flex-col items-start gap-2">
+                <label className="text-sm text-gray-700 font-medium">Lichtintensiteit</label>
+                <input
+                type="range"
+                min="0"
+                max="8"
+                step="0.1"
+                value={intensity}
+                onChange={(e) => setIntensity(e.target.value)}
+                className="w-full h-2 rounded-md border border-gray-300 shadow-inner cursor-pointer transition hover:scale-[1.02]"
+                />
+                </div>
         </div>
 
 
       {/* 3D model viewer */}
       <div className="flex-1">
         <Canvas camera={{ position: [0, 2, 6], fov: 45 }}>
-          <ambientLight intensity={1} />
-          <directionalLight position={[3, 3, 3]} intensity={1.5} />
+          <ambientLight intensity={1.5} />
+          <directionalLight position={[3, 3, 3]} intensity={intensity} />
           <Suspense fallback={null}>
             <WomanModel
               skinColor={skinColor}
@@ -58,7 +72,7 @@ export default function ModelPage() {
               eyeColor={eyeColor}
               scale={1.5}
               position={[0, 0, 0]}
-              rotation={[0, Math.PI, 0]}
+              rotation={[-0.35, 0, 0]}
             />
           </Suspense>
           <OrbitControls />
