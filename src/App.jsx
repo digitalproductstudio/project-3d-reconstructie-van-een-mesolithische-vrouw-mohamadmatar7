9d47075f44b2ac814a45941e01ec142573493538
karimmatar7 <karim.matar@student.arteveldehs.be>
2025-04-28 14:14:00 +0200

    fix hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ModelPage from './pages/ModelPage';
 import ARPage from './pages/ARPage';
