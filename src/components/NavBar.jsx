7deee1d17d4310e9e0028e12bb2a8bdf9f243f21
mohamadmatar7 <mohamad.matar@student.arteveldehs.be>
2025-05-19 11:52:45 +0200

    enhance link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,15 +10,17 @@ export default function NavBar() {
           { to: '/ar', label: 'AR' },
           { to: '/facts', label: 'Facts' },
         ].map((item) => (
-          <Link
-            key={item.to}
-            to={item.to}
-            className="relative text-[#5C3A1E] font-medium hover:text-[#8B5A2B] transition"
-          >
-            <span className="after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-[#8B5A2B] after:transition-all after:duration-300 hover:after:w-full">
-              {item.label}
-            </span>
-          </Link>
+        <Link
+          key={item.to}
+          to={item.to}
+          className="relative text-[#5C3A1E] font-medium text-lg md:text-xl hover:text-[#8B5A2B] transition mt-4"
+          style={{ textShadow: '0.5px 0.5px 1px rgba(0, 0, 0, 0.25)' }}
+        >
+          <span className="after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-[#8B5A2B] after:transition-all after:duration-300 hover:after:w-full">
+            {item.label}
+          </span>
+        </Link>
+
         ))}
       </div>
     </nav>
